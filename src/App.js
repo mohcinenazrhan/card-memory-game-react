0cd247db7f25daf1d064c151d7f090e66abd5d70
Mohcine NAZRHANE <mohcine@gmail.com>
2018-10-20 10:34:05 +0000

    feat: make the game functional
    - add getFeedbackForCard func to get feedback of the card dynamically
    - update handleCardClick and implemente handleNewPairClosedBy funcs to update the state according to the cases
    - HallOfFame component still static

--- src/App.js
+++ src/App.js
@@ -9,10 +9,31 @@ import HallOfFame, { FAKE_HOF } from './components/halloffame/HallOfFame'
 
 const SIDE = 6
 const SYMBOLS = '🎃🎂🎅🐰🎥🍂👨💪🎓👩🎊🏊👑☪🌱☘☀🏈💘⚽'
+const VISUAL_PAUSE_MSECS = 750
 
 class App extends Component {
-  cards = this.generateCards()
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: []
+  }
 
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
+  }
+  
   generateCards() {
     const result = []
     const size = SIDE * SIDE
@@ -24,20 +45,47 @@ class App extends Component {
     return shuffle(result)
   }
 
-  handleCardClick(card) {
-    console.log(card, 'clicked')
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
   }
 
   render() {
-    const won = new Date().getSeconds() % 2 === 0
+    const { cards, guesses, matchedCardIndices } = this.state
+    const won = matchedCardIndices.length === cards.length
     return (
       <div className="memory">
-        <GuessCount guesses={0} />
-        {this.cards.map((card, index) => (
+        <GuessCount guesses={guesses} />
+        {cards.map((card, index) => (
           <Card
             card={card}
-            feedback="visible"
+            feedback={this.getFeedbackForCard(index)}
             key={index}
+            index={index}
             onClick={this.handleCardClick}
           />
         ))}
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,8 +5,8 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback, onClick }) => (
-    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+const Card = ({ card, feedback, index, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(index)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>
@@ -14,14 +14,15 @@ const Card = ({ card, feedback, onClick }) => (
 )
 
 Card.propTypes = {
-  card: PropTypes.string.isRequired,
-  feedback: PropTypes.oneOf([
-    'hidden',
-    'justMatched',
-    'justMismatched',
-    'visible',
-  ]).isRequired,
-  onClick: PropTypes.func.isRequired,
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'justMismatched',
+        'visible',
+    ]).isRequired,
+    index: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
 }
 
 export default Card
