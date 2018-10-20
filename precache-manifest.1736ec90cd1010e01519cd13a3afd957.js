3618342c307a3444dd52e5c779b41ee1d24a6503
Mohcine NAZRHANE <mohcine@gmail.com>
2018-10-20 21:13:05 +0000

    chore: prepare the app to deploy
    - Add homepage to package.json file
    - Allow git to add build folder

---
+++ precache-manifest.1736ec90cd1010e01519cd13a3afd957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34ae79358f7552e0ca78",
+    "url": "/static/js/main.34ae7935.chunk.js"
+  },
+  {
+    "revision": "8050e7917bb218d3eaae",
+    "url": "/static/js/1.8050e791.chunk.js"
+  },
+  {
+    "revision": "34ae79358f7552e0ca78",
+    "url": "/static/css/main.1cb00b5d.chunk.css"
+  },
+  {
+    "revision": "9e166a16a948a6dbd8439aad2d3546b3",
+    "url": "/index.html"
+  }
+];
