82d370e7a2f72c75dfabdaca42e78415940f0508
Mohcine NAZRHANE <mohcine@gmail.com>
2018-10-20 21:51:51 +0000

    fix: make path in index.html relative
    - fix homepage in package.json file

---
+++ build/precache-manifest.9efe31fd11060d9338d0f99b0d576597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ba2c876c751f4684ccd",
+    "url": "/card-memory-game-react/static/js/runtime~main.1ba2c876.js"
+  },
+  {
+    "revision": "f9ea46e01ac0d3508fa6",
+    "url": "/card-memory-game-react/static/js/main.f9ea46e0.chunk.js"
+  },
+  {
+    "revision": "8050e7917bb218d3eaae",
+    "url": "/card-memory-game-react/static/js/1.8050e791.chunk.js"
+  },
+  {
+    "revision": "f9ea46e01ac0d3508fa6",
+    "url": "/card-memory-game-react/static/css/main.1cb00b5d.chunk.css"
+  },
+  {
+    "revision": "6c5e9d13f20f60565ee365a4a8940427",
+    "url": "/card-memory-game-react/index.html"
+  }
+];
