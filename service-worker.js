30c8366b02900a912bbe9c5c9e9a998fc1e56d80
Mohcine NAZRHANE <mohcine@gmail.com>
2018-10-20 21:51:51 +0000

    fix: make path in index.html relative
    - fix homepage in package.json file

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1736ec90cd1010e01519cd13a3afd957.js"
+  "/card-memory-game-react/precache-manifest.9efe31fd11060d9338d0f99b0d576597.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/card-memory-game-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
